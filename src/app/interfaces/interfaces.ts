8180dfe5c287bea06295a7bc4c9a2090b0a70428
Dicson <fetex98@gmail.com>
2020-06-17 12:12:10 -0500

    Create Interfaces for Data

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,22 @@
+ export interface ResponseTopHeadlines {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+}
+
+ export interface Article {
+    source: Source;
+    author?: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publisheadAt: string;
+    content?: string;
+}
+
+
+ export interface Source {
+    id?: string;
+    name: string;
+}
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ResponseTopHeadlines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class NewsService {
   constructor( private http: HttpClient) { }
 
   getTopHeadlines(){
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=5ff30d4ec6aa463086f3cb32e22744fb`);
+    return this.http.get<ResponseTopHeadlines>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=5ff30d4ec6aa463086f3cb32e22744fb`);
 
   }
 }
