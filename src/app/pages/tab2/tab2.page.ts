32c930e7666cb944330e749d665fca70202664f1
Dicson <fetex98@gmail.com>
2020-06-17 20:35:51 -0500

    show news for  category

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NewsService } from '../../services/news.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -9,18 +10,30 @@ import { NewsService } from '../../services/news.service';
 })
 export class Tab2Page implements OnInit {
 
+
   @ViewChild(IonSegment, {static: true})  segment: IonSegment;
   categories =  ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  notices: Article[] = [];
 
   constructor( private newService: NewsService){
 
   }
 
   ngOnInit(){
-    this.segment.value = this.categories[0];
-    this.newService.getTopHeadlinesCategory( this.categories[0])
+    this.segment.value = this.categories[0];  /* Value for defaul in the segment*/
+    this.loadNews( this.categories[0]);
+  }
+  //
+  changeCategory( event ){
+    this.notices = [];
+    this.loadNews( event.detail.value);  /* Update Value of segment */
+  }
+  /* Function for load news for category*/
+  loadNews( category: string){
+    this.newService.getTopHeadlinesCategory( category)
     .subscribe( resp => {
       console.log(resp);
+      this.notices.push(...resp.articles);
     });
   }
 }
