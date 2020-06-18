8b7b205a942404efb7ae67c722c3fbe0f071f580
Dicson <fetex98@gmail.com>
2020-06-17 23:00:02 -0500

    action-sheet share,favorite and cancel

--- src/app/components/new/new.component.ts
+++ src/app/components/new/new.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
+import { ActionSheetController } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
@@ -13,7 +14,8 @@ export class NewComponent implements OnInit {
   @Input() new: Article;
   @Input() index: number;
 
-  constructor( private iab: InAppBrowser) { }
+  constructor( private iab: InAppBrowser,
+               private actionSheetCtlr: ActionSheetController ) { }
 
   ngOnInit() {}
 
@@ -21,4 +23,34 @@ export class NewComponent implements OnInit {
     const browser = this.iab.create(this.new.url, '_system');
   }
 
+  async  menuOptions(){
+    const actionSheet = await this.actionSheetCtlr.create({
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+        text: 'Share',
+        icon: 'share-social-outline',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        cssClass: 'action-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
